3467f7dcf030a99876519cdcfa0ab754c27661e5
Kimtaegi4612@gmail.com <Kimtaegi4612>
2025-01-07 14:11:22 +0900

    Feat: Java 흐름제어 2-조건문

--- java/JavaBasic/src/Test3.java
+++ java/JavaBasic/src/Test3.java
@@ -18,11 +18,19 @@ public class Test3 {
         // 1. 콘솔 입력
         Scanner sc = new Scanner(System.in); // 콘솔에서 사용자 입력 대기하는 코드
         while (true) {
-
+            System.out.println("Enter number: ");
             // 2. 사용자 입력후 앤터 => 값이 반환
-            int userInputValue = sc.nextInt();
+            int userInputValue = sc.nextInt(); // 입력 대기
             // 3. 만약 100을 입력했다면, 종료 혹은 반복문 탈출
             if (userInputValue == 100) break;
+            // 4. 조건식 사용
+            if (userInputValue > 10 ){
+                System.out.println("10보다 초과한다.");
+            }else{
+                System.out.println("10보다 작다");
+            }
         }
+        // 입력 닫기
+        sc.close();
     }
 }
