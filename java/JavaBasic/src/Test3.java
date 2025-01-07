805b21234ae9231e56d9bee8e6affbd51ef24a1a
Kimtaegi4612@gmail.com <Kimtaegi4612>
2025-01-07 13:40:50 +0900

    updated

--- java/JavaBasic/src/Test3.java
+++ java/JavaBasic/src/Test3.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
+/*
+*   흐름제어
+*   - 조건문 : if ~ else if ~else ~
+*       - 콘솔 입력 사용
+*   - 반복문 : for~, do ~while, while ~, continuem break
+*   - 게임 제작(간단)
+*       - 콘솔 입력 사용
+*
+** */
 public class Test3 {
 }
