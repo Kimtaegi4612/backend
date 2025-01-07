98e26e9f7c6f74127297373c082b495cf3946c95
Kimtaegi4612@gmail.com <Kimtaegi4612>
2025-01-07 13:40:23 +0900

    Feat: Java 흐름제어 조건문 반복문

--- java/JavaBasic/src/Test2.java
+++ java/JavaBasic/src/Test2.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
-
+/*
+* 자바의 타입
+* - 참조형
+*       - 대표적인 종류 : 문자열(String)
+* */
 public class Test2 {
+    public static void main(String[] args) {
+
+        // 문자열 테스트
+        // 타입변수 = 값
+        String news = "크리스마스"; //문자열은 ""
+        System.out.println(news);
+
+        // 배열 => 아파트 : 대표변수명 [위치정보:인덱스]
+        // 정수열 배열이라고 표현 멤버는 정수만 가질 수 있다.
+        // 자바는 타입을 강력하게 제한한다
+        int[] numbers = {1,2,3,4,5,6,7,8,9,10}; // 배열 numbers의 구성원은 무조건 int 타입만
+        System.out.println(numbers[0]);
+        System.out.println(numbers[5]);
+
+        // 문자열 배열
+        String[] langs = {"Java","JS","Python"}; // 멤버는 무조건 문자열만 추가
+        System.out.println(langs[0]);
+        System.out.println(langs[1]);
+        // JS의 배열과 문법적으로는 생성을 제외하고는 유사하다.
+    }
 }
