f36fb8b66e37f7097e5859f8e6aff6681a237bf4
Kimtaegi4612@gmail.com <Kimtaegi4612>
2025-01-07 14:07:03 +0900

    Feat: Java 흐름제어 1

--- java/JavaBasic/src/Test3.java
+++ java/JavaBasic/src/Test3.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
 *   흐름제어
 *   - 조건문 : if ~ else if ~else ~
@@ -8,4 +10,19 @@
 *
 ** */
 public class Test3 {
+    public static void main(String[] args) {
+        //조건문 연습
+        flowcontrol1();
+    }
+    private static void flowcontrol1() {
+        // 1. 콘솔 입력
+        Scanner sc = new Scanner(System.in); // 콘솔에서 사용자 입력 대기하는 코드
+        while (true) {
+
+            // 2. 사용자 입력후 앤터 => 값이 반환
+            int userInputValue = sc.nextInt();
+            // 3. 만약 100을 입력했다면, 종료 혹은 반복문 탈출
+            if (userInputValue == 100) break;
+        }
+    }
 }
