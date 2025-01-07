c0f08dcdbb74389a6654e116b28048d648ee8de5
Kimtaegi4612@gmail.com <Kimtaegi4612>
2025-01-08 05:58:21 +0900

    Feat: SB Post전송 + URL path

--- springboot/demoex/src/main/java/org/example/demoex/controllers/HomeController.java
+++ springboot/demoex/src/main/java/org/example/demoex/controllers/HomeController.java
@@ -1,10 +1,7 @@
 package org.example.demoex.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
@@ -15,11 +12,10 @@ public class HomeController {
 
         return "index"; // View Resolver가 "index.html" 또는 "index.jsp"를 찾음
     }
-    // 2. 직접 복잡한 URL 구성, get (메소드 항상 기제)
-    // prefix 무시 건건히 사용
+
+    // 2. 직접 복잡한 URL 구성, get
+    // prefix 무시!! 건건이 사용
     // ex) ~/test/news/sports
-
-
     @GetMapping("/test/news/sports")
     public String sports() {
         return "sports";
@@ -52,3 +48,5 @@ public class HomeController {
                         @RequestParam(value = "servicetype") String servicetype) {
         return nid + " <- news " + id + " " + servicetype;
     }
+
+}
